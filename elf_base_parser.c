a35082e372d32ffdb7cfa381b9dafad2d1182cf7
Async-Mayeul <mayeulfargier10@gmail.com>
2024-04-16 21:28:20 +0200

    add definition of read_elf_file in elf_base_parser.c

--- elf_base_parser.c
+++ elf_base_parser.c
@@ -1,3 +1,22 @@
-#include "elf_base_parser.h"
+#include <elf_base_parser.h>
 
-Elf64_Ehdr* read_elf_file(const char* fileName);
+Elf64_Ehdr* read_elf_file(const char* fileName) {
+    struct Elf64_Ehdr elf_header;
+    FILE* elf_file = fopen(fileName, "rb");
+
+    if(elf_file) {
+        fread(&elf_header, 1, sizeof(elf_header), elf_file);
+
+        if(elf_header.e_type == 0x7f &&
+            elf_header->e_ident[1] == 'E' &&
+            elf_header->e_ident[2] == 'L' &&
+            elf_header->e_ident[3] == 'F') {
+
+            printf("It's an ELF file");
+        } else {
+            printf("Error it's not an ELF file");
+        }
+    }
+
+    return elf_header;
+}
--- elf_base_parser.h
+++ elf_base_parser.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <elf.h>
 
 #ifndef SIMPLE_ELF
